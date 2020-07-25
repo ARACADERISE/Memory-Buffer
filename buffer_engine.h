0e6a6b44b5d85dd52540054967073bdf45ffb8fb
GetStuffDone <t.targetyt@gmail.com>
2020-07-25 17:55:37 +0000

    removed useless macros

--- buffer_engine.h
+++ buffer_engine.h
@@ -6,12 +6,3 @@ typedef struct BUFFER {
 } buffer;
 
 #endif
-/* We should be all setup by this point */
-#ifdef BUFFER_ENGINE
-#define BUFFER_ENGINE_
-
-typedef struct BUFFER_ {
-
-} buffer_;
-
-#endif
